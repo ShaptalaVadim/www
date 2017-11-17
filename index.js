1b2313e4ef17efc85791d76b65c2742e38c4dd5c
Vadim Shaptala <ShaptalaVadim@yandex.ru>
2017-11-17 18:43:58 +0300

    init

--- index.js
+++ index.js
@@ -1,26 +1,112 @@
-﻿var express = require("express");
+﻿var express = require("express");   
 const MongoClient    = require('mongodb').MongoClient;
 
 
-var app = express();
-//app.use(express.logger());
-
-var url = "mongodb://piter:petro123@ds141454.mlab.com:41454/firstdb1"
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
 
-app.get('/', function(request, response) {
-  
-MongoClient.connect(url, function(err, db){
-        db.collection("students").find({}).toArray(function(err, users){
-            response.send(users)
+// создаем приложение
+var app = express();                
+//var url = "mongodb://localhost:27017/dbtest";
+var url = "mongodb://piter:petro123@ds141454.mlab.com:41454/firstdb1";
+app.use(express.static(__dirname + "/public/Build html documentation/"));
+
+
+var addRes = function(request, response){
+console.log("aaaa");
+var numQuest = request.body.num;
+var res = request.body.ansvers;
+console.log(res);
+    MongoClient.connect(url, function(err, db){
+       
+ db.collection("test").findOneAndUpdate({num: numQuest}, {$set:{a2:res}}, {returnOriginal:false}, function(err, quest){
+             
+            if(err) return response.status(400).send();
+            console.log(quest); 
             db.close();
-            console.log(users);
-        });
-    });
+response.send(quest.value);
+
+});
+});
+}
+
+
+
+
+
+
+app.post("/api/res",jsonParser,addRes);
+
+
+app.get("/api/cntQw/", function (request, response) {
+     
+
+    MongoClient.connect(url, function(err, db){
+       
+    db.collection("test").count({}, function(err, n){
+             
+            if(err) return response.status(400).send();
+        
+            db.close();
+  
+
+    response.send(String(n));
+  });
 
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
 });
+
+
+app.get("/api/getResult/", function (request, response) {
+     
+
+    MongoClient.connect(url, function(err, db){
+       
+
+ db.collection("test").find({}).toArray(function(err, quest){
+             
+            if(err) return response.status(400).send();
+             
+            db.close();
+  
+
+    response.send(quest);
+ 
+
+
+});
+
+ });
+
+
+
+});
+
+
+
+
+
+
+app.get("/api/answer/:num", function (request, response) {
+    console.log(request.params.num); 
+var questionNum = parseInt(request.params.num);
+    MongoClient.connect(url, function(err, db){
+       
+
+ db.collection("test").findOne({num: questionNum}, function(err, quest){
+             
+            if(err) return response.status(400).send();
+          
+            db.close();
+  
+
+    response.send(quest);
+  });
+
+});
+
+});
+
+app.listen(3000);
---
+++ index2.js
@@ -0,0 +1,26 @@
+﻿var express = require("express");
+const MongoClient    = require('mongodb').MongoClient;
+
+
+var app = express();
+//app.use(express.logger());
+
+var url = "mongodb://piter:petro123@ds141454.mlab.com:41454/firstdb1"
+
+
+app.get('/', function(request, response) {
+  
+MongoClient.connect(url, function(err, db){
+        db.collection("students").find({}).toArray(function(err, users){
+            response.send(users)
+            db.close();
+            console.log(users);
+        });
+    });
+
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
